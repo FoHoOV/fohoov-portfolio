558a16d661c7d20bcdc4a700811e28911c98ff3a
godzylinux <fohoov@gmail.com>
2024-05-24 01:02:27 +0330

    added sunset theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,16 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkMode: ['class', '[data-theme="dark"]'],
+	darkMode: ['class', '[data-theme="sunset"]'],
 	plugins: [typography(), daisyui],
 	daisyui: {
-		themes: ['light', 'dark']
+		themes: ['sunset', 'light'],
+		defaultTheme: "sunset"
 	},
 	theme: {
 		screens: {
 			xs: '475px',
 			...defaultTheme.screens
-		}
+		},
 	},
 };
