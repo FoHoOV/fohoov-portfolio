e97f9b218714d491ff2cf465268c897b072b9171
godzylinux <fohoov@gmail.com>
2024-05-24 01:10:56 +0330

    added my name xD

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="flex h-full w-full items-center justify-center gap-2 text-7xl">
+	{#each 'FoHoOV' as character, i}
+		<span class:translate-y-2={i % 2 == 0} class:translate-y-5={i % 2 == 1}>
+			{character}
+		</span>
+	{/each}
+</p>
