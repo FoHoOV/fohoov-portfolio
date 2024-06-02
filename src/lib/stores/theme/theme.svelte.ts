69250f89e330c3192d26f5b255df70f6d8626c08
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-02 20:17:13 +0330

    persisting theme to cookies

--- src/lib/stores/theme/theme.svelte.ts
+++ src/lib/stores/theme/theme.svelte.ts
@@ -1,13 +1,20 @@
 import { browser } from '$app/environment';
 import { CACHED_THEME_KEY_NAME } from './constants';
 import { persisted } from '$lib/stores';
+import Cookies from "js-cookie";
 
 const _storedTheme = persisted.primitive$(CACHED_THEME_KEY_NAME, null);
 
 export function getSelectedTheme$() {
 	const theme = $derived.by(() => {
 		if (!browser) {
-			return 'dark';
+			let cookieThemeName = Cookies.get(CACHED_THEME_KEY_NAME);
+
+			if(!cookieThemeName) {
+				cookieThemeName = "dark";
+				Cookies.set(CACHED_THEME_KEY_NAME, cookieThemeName);
+			}
+			return cookieThemeName;
 		}
 		if (
 			_storedTheme.current === 'light' ||
@@ -28,4 +35,5 @@ export function getSelectedTheme$() {
 
 export function setTheme(theme: 'dark' | 'light') {
 	_storedTheme.current = theme;
+	Cookies.set(CACHED_THEME_KEY_NAME, theme); 
 }
