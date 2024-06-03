1957868f23e1dbdf76d1fe5022b2821c103dd33e
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-03 22:28:19 +0330

    added grid effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,28 @@
 <script lang="ts" context="module">
 	import { browser } from '$app/environment';
 	import { gsap } from 'gsap';
-</script>
+	import { onMount } from 'svelte';
 
-<script lang="ts">
-	function gsapTo(
-		node: HTMLElement,
-		options: gsap.TweenVars & {
-			timeLine?: gsap.core.Timeline;
-			instance?: (instance: gsap.core.Tween | gsap.core.Timeline) => void;
-		}
-	) {
+	// TODO: create this and grid effects into an action
+	type GsapOptions = gsap.TweenVars & {
+		/**
+		 * timeline instance to use instead of gsap.*
+		 */
+		timeLine?: gsap.core.Timeline;
+		/**
+		 * calls the callback with the instance its using
+		 */
+		instance?: (instance: gsap.core.Tween | gsap.core.Timeline) => void;
+	};
+
+	function gsapTo(node: HTMLElement, options: GsapOptions) {
 		const res = (options.timeLine ?? gsap).to(node, options);
 		if (options.instance) {
 			options.instance(res);
 		}
 	}
 
-	function gsapFrom(
-		node: HTMLElement,
-		options: gsap.TweenVars & {
-			timeLine?: gsap.core.Timeline;
-			instance?: (instance: gsap.core.Tween | gsap.core.Timeline) => void;
-		}
-	) {
+	function gsapFrom(node: HTMLElement, options: GsapOptions) {
 		const res = (options.timeLine ?? gsap).from(node, options);
 		if (options.instance) {
 			options.instance(res);
@@ -31,12 +30,48 @@
 	}
 </script>
 
-<div class="flex h-full w-full items-center justify-center gap-2 overflow-hidden text-7xl">
+<script lang="ts">
+	let wrapper: HTMLElement;
+	function createGridEffect() {
+		const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.5 });
+		tl.from(wrapper.querySelectorAll('.box'), {
+			duration: 1,
+			scale: 0,
+			yoyo: true,
+			repeat: -1,
+			repeatDelay: 2,
+			ease: 'power1.inOut',
+			stagger: {
+				amount: 0.5,
+				grid: [3, 3],
+				ease: 'power2.in',
+				from: 'center'
+			}
+		});
+	}
+
+	onMount(() => {
+		createGridEffect();
+	});
+</script>
+
+<div class="relative flex h-full w-full items-center justify-center gap-2 overflow-hidden text-7xl">
+	<div
+		bind:this={wrapper}
+		class="absolute left-0 top-0 -z-10 grid h-full w-full grid-cols-3 grid-rows-3 bg-base-200"
+	>
+		{#each { length: 3 } as _, i}
+			{#each { length: 3 } as _, i}
+				<span class="box h-full w-full bg-base-300"> </span>
+			{/each}
+		{/each}
+	</div>
+
 	{#each 'FoHoOV' as character, i}
 		<span
 			class="invisible"
 			use:gsapTo={{
-				yPercent: 200,
+				yPercent: 300,
 				xPercent: Math.pow(i * 5, 2),
 				opacity: 0,
 				scrollTrigger: {
@@ -46,8 +81,9 @@
 				}
 			}}
 			use:gsapFrom={{
-				yPercent: -200,
+				yPercent: -300,
 				xPercent: Math.pow(i * 5, 2),
+				scale: 3,
 				duration: 1,
 				autoAlpha: 0
 			}}
