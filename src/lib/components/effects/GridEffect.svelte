c5f2971ca35f9e40153e45e02e0ff6edeff1e2c6
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-04 23:15:34 +0330

    separated grid effect and text effect to components

---
+++ src/lib/components/effects/GridEffect.svelte
@@ -0,0 +1,85 @@
+<script lang="ts" context="module">
+	import { gsapCreator, type TimeLineCreator } from '$lib';
+	import type { Snippet } from 'svelte';
+
+	export type Props = {
+		children?: Snippet;
+		class?: string;
+	};
+</script>
+
+<script lang="ts">
+	const { class: className, children }: Props = $props();
+
+	function createGridEffect(wrapper: HTMLElement, getTimeline: TimeLineCreator) {
+		const boxes = wrapper.querySelectorAll('.box');
+		const boxesTimeLine = getTimeline({
+			label: 'boxesTimeLine'
+		});
+		boxesTimeLine.from(boxes, {
+			duration: 1,
+			scale: 0,
+			stagger: {
+				amount: 0.5,
+				grid: [3, 3],
+				ease: 'power3.inOut',
+				from: 'center'
+			}
+		});
+	}
+
+	function shiftGridBorders(wrapper: HTMLElement, getTimeline: TimeLineCreator) {
+		const boxes = wrapper.querySelectorAll('.box');
+		const boxesTimeLine = getTimeline({
+			label: 'boxesTimeLine'
+		});
+
+		const inner = () => {
+			boxesTimeLine.to(boxes, {
+				duration: 2,
+				yoyo: true,
+				borderWidth: '1px',
+				borderStyle: 'solid',
+				borderColor: '#8080804a',
+				scale: 1.1,
+				repeat: 1,
+				transformOrigin: 'center',
+				stagger: {
+					amount: 1,
+					grid: [3, 3],
+					ease: 'power3.inOut',
+					from: 'random'
+				},
+				onComplete: () => {
+					setTimeout(() => {
+						inner();
+					}, 5000);
+				}
+			});
+		};
+
+		inner();
+	}
+</script>
+
+<div class={`relative ${className}`}>
+	<div
+		use:gsapCreator={[
+			({ target, getTimeline: getTimeline }) => {
+				createGridEffect(target, getTimeline);
+			},
+			({ target, getTimeline: getTimeline }) => {
+				shiftGridBorders(target, getTimeline);
+			}
+		]}
+		class="absolute left-0 top-0 -z-10 grid h-full w-full grid-cols-3 grid-rows-3 bg-base-200"
+	>
+		{#each { length: 3 } as _}
+			{#each { length: 3 } as _}
+				<span class="box h-full w-full border-0 border-transparent bg-base-300"> </span>
+			{/each}
+		{/each}
+	</div>
+
+	{@render children?.()}
+</div>
---
+++ src/lib/components/effects/TextEffect.svelte
@@ -0,0 +1,63 @@
+<script lang="ts" context="module">
+	import { gsap } from 'gsap';
+	import { gsapCreator, type TimeLineCreator } from '$lib';
+
+	export type Props = {
+		text: string;
+	};
+</script>
+
+<script lang="ts">
+	const { text }: Props = $props();
+
+	function createNameOutroEffect(index: number, wrapper: HTMLElement) {
+		gsap.to(wrapper, {
+			yPercent: 300,
+			xPercent: Math.pow(index * 5, 2),
+			opacity: 0,
+			scrollTrigger: {
+				scrub: true,
+				start: 'center center',
+				end: 'bottom center'
+			}
+		});
+	}
+
+	function createNameIntroEffect(
+		index: number,
+		wrapper: HTMLElement,
+		getTimeline: TimeLineCreator
+	) {
+		const timeLine = getTimeline();
+		timeLine.from(wrapper, {
+			yPercent: -300,
+			xPercent: Math.pow(index * 5, 2),
+			scale: 3,
+			duration: 1,
+			autoAlpha: 0
+		});
+		timeLine.to(wrapper, {
+			textShadow: 'rgba(245,242,237,1) 0px 0px 300px',
+			yoyo: true,
+			repeat: -1,
+			duration: 3
+		});
+	}
+</script>
+
+{#each text as character, i}
+	<span
+		class="invisible"
+		use:gsapCreator={[
+			({ target, getTimeline: getTimeline }) => {
+				createNameIntroEffect(i, target, getTimeline);
+			},
+			({ target }) => {
+				createNameOutroEffect(i, target);
+			}
+		]}
+		style="text-shadow: rgba(245,242,237,0.2) 0px 0px 10px"
+	>
+		{character}
+	</span>
+{/each}
