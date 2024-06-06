93ffda6e63c76575dd8a3a304217b7c1f37e7028
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-06 21:51:01 +0330

    reduced spread value so that chrome doesn't choke that hard on animations

--- src/lib/components/effects/GlowEffect.svelte
+++ src/lib/components/effects/GlowEffect.svelte
@@ -16,7 +16,9 @@
 
 <style>
 	.glow-effect {
-		text-shadow: rgba(245, 242, 237, 0.2) 0px 0px 50px;
+		--initial-value: rgba(245, 242, 237, 0.2) 0px 0px 50px;
+		--to-value: rgba(245, 242, 237, 1) 0px 0px 150px;
+		text-shadow: var(--initial-value);
 		animation-name: animate-glow-effect;
 		animation-duration: 6s;
 		animation-fill-mode: forwards;
@@ -25,10 +27,10 @@
 	}
 	@keyframes animate-glow-effect {
 		50% {
-			text-shadow: rgba(245, 242, 237, 1) 0px 0px 300px;
+			text-shadow: var(--to-value);
 		}
 		100% {
-			text-shadow: rgba(245, 242, 237, 0.2) 0px 0px 50px;
+			text-shadow: var(--initial-value);
 		}
 	}
 </style>
