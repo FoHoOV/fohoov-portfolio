4705254a516f0bc6f5eb4e06b0f19b2ee49ab2f8
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-09 00:48:03 +0330

    init using threlte to add some cubes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,19 @@
 	import FoHoOV from '$lib/components/home/FoHoOV.svelte';
 	import WhatIKnow from '$lib/components/home/WhatIKnow.svelte';
 	import Mastering from '$lib/components/home/Mastering.svelte';
+	import Projects from '$lib/components/home/Projects.svelte';
+	import Scene from '$lib/components/threlte-scenes/Scene.svelte';
+	import { Canvas } from '@threlte/core';
 </script>
 
 <script lang="ts">
-	import Projects from '$lib/components/home/Projects.svelte';
 </script>
+
+<div class="fixed left-0 top-0 h-full w-full">
+	<Canvas>
+		<Scene></Scene>
+	</Canvas>
+</div>
 
 <FoHoOV />
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [{ ...threeMinifier(), enforce: 'pre' }, sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
