576e11c7d7730fa892cca2ee465d5cff7d39e408
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-07-19 12:07:21 +0330

    chore: removed exporting internal symbols

--- src/lib/stores/theme/context.ts
+++ src/lib/stores/theme/context.ts
@@ -1,7 +1,7 @@
 import { getContext, setContext } from '$lib/stores';
 import type { ThemeManager } from './theme.svelte';
 
-export const THEME_CONTEXT_KEY = Symbol();
+const THEME_CONTEXT_KEY = Symbol();
 
 export function getTheme() {
 	return getContext<ThemeManager>(THEME_CONTEXT_KEY);
