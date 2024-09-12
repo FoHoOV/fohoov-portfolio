b6dfbccd5d4c9a2b2de7dae9bdc33027466a34b0
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-09-12 14:15:59 +0330

    chore: export different css values individually

--- src/lib/utils/types/css-value.ts
+++ src/lib/utils/types/css-value.ts
@@ -1,7 +1,6 @@
-export type CssValue =
-	| `${number}`
-	| `${number}px`
-	| `${number}rem`
-	| `${number}em`
-	| `${number}%`
-	| `${number}deg`;
+export type PixelSize = `${number}px`;
+export type RemSize = `${number}rem`;
+export type EmSize = `${number}em`;
+export type PercentageSize = `${number}%`;
+export type DegreeSize = `${number}deg`;
+export type Size = `${number}` | PixelSize | RemSize | EmSize | PercentageSize | DegreeSize;
