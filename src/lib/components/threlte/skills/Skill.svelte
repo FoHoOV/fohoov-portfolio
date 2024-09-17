042b8c6eb7569595a94e9f2cac77f8f7dc94c10a
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-09-17 06:13:00 +0330

    feat: added text animation

--- src/lib/components/threlte/skills/Skill.svelte
+++ src/lib/components/threlte/skills/Skill.svelte
@@ -2,16 +2,34 @@
 	import SphereWithImage, {
 		type Props as SphereWithImageProps
 	} from '$lib/components/threlte/sphere/SphereWithImage.svelte';
+	import { Text } from '@threlte/extras';
 
 	import { spring } from 'svelte/motion';
 	import { fromStore } from 'svelte/store';
+	import type { Color, ColorRepresentation } from 'three';
 
-	export type Props = { scale?: never } & SphereWithImageProps;
+	export type Props = {
+		text: string;
+		fontSize: number;
+		textColor?: Color | ColorRepresentation;
+		scale?: never;
+	} & SphereWithImageProps;
 </script>
 
 <script lang="ts">
-	let { ref = $bindable(undefined), ...restProps }: Props = $props();
+	let {
+		text,
+		fontSize,
+		textColor = 'white',
+		distanceFromSphere = 0.3,
+		rotationSpeed = 0,
+		ref = $bindable(undefined),
+		children,
+		...restProps
+	}: Props = $props();
 
+	const rotation = fromStore(spring<number>(0));
+	const fillOpacity = $derived(Math.PI / rotation.current);
 	const zoom = fromStore(spring<number>(1));
 </script>
 
@@ -20,9 +38,23 @@
 	scale={zoom.current}
 	onpointerenter={(e: unknown) => {
 		zoom.current = 12 / 10;
+		rotation.current = Math.PI;
 	}}
 	onpointerleave={(e: unknown) => {
 		zoom.current = 10 / 12;
+		rotation.current = 0;
 	}}
+	xRotation={rotation.current}
+	{rotationSpeed}
 	{...restProps}>
+	<Text
+		position={[0, 0, -restProps.radius - distanceFromSphere]}
+		{text}
+		{fontSize}
+		curveRadius={restProps.radius + distanceFromSphere}
+		anchorY={'50%'}
+		anchorX={'50%'}
+		{fillOpacity}
+		color={textColor} />
+	{@render children?.()}
 </SphereWithImage>
