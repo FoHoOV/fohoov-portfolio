bad768d352f67820497d154ba9a5a3443873f5f3
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-09-29 09:02:16 +0330

    feat: added rotational light

---
+++ src/lib/components/threlte/light/ RotationalLight.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" module>
+	import { T, type Props as ThrelteProps } from '@threlte/core';
+	import { onMount } from 'svelte';
+	import { Object3D } from 'three';
+	import type { SpotLight } from 'three/src/lights/SpotLight.js';
+	import type { Vector3, Vector3Like } from 'three/src/math/Vector3.js';
+
+	export type Props = {
+		position?: Vector3 | Vector3Like | [x: number, y: number, z: number];
+		rotation?: Vector3Like;
+	} & ThrelteProps<SpotLight>;
+</script>
+
+<script lang="ts">
+	const { position, rotation, ...lightProps }: Props = $props();
+
+	let objRef: Object3D | undefined = undefined;
+	let spotlightRef: SpotLight | undefined = undefined;
+
+	onMount(() => {
+		spotlightRef!.target = objRef!;
+	});
+</script>
+
+<T.Group {position}>
+	<T.Object3D bind:ref={objRef} position.z={-1} {rotation}></T.Object3D>
+	<T.SpotLight bind:ref={spotlightRef} position={[0, 0, 0]} {...lightProps}></T.SpotLight>
+</T.Group>
