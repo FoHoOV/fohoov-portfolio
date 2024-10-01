71b5491d68c40442a06c68aeff7d310fe0f9e6a6
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-10-01 20:48:28 +0330

    fix: removed three minifier to resolve threlte runtime issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 
 export default defineConfig({
-	plugins: [{ ...threeMinifier(), enforce: 'pre' }, sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
