727717df3c844a974008bfc8ea4310c57613fa3d
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-03 22:49:55 +0330

    fix: no overlap for for projects when in mobile mod

--- src/lib/components/threlte/projects/FlashLight.svelte
+++ src/lib/components/threlte/projects/FlashLight.svelte
@@ -6,13 +6,17 @@
 	import { fromStore } from 'svelte/store';
 	import RotationalLight from '$lib/components/threlte/light/RotationalLight.svelte';
 	import { Vector3, type Group, type Object3D } from 'three';
+
+	export type Props = {
+		axis: 'x' | 'y';
+	};
 </script>
 
 <script lang="ts">
+	const { axis }: Props = $props();
+
 	let flashLightRef: Group | undefined = undefined;
-
 	const spotLightAngel = 1.2;
-
 	const initialScale = 15;
 	const scale = fromStore(
 		spring(0, {
@@ -21,7 +25,7 @@
 	);
 
 	const { pointer } = useInteractivity();
-	const spotLightYRotation = $derived(-$pointer.x * 1.5);
+	const spotLightRotation = $derived((axis === 'x' ? -$pointer.x : $pointer.y) * 1.5);
 
 	function startDraggingState() {
 		scale.current = initialScale;
@@ -60,11 +64,15 @@
 
 <FlashLight
 	bind:ref={flashLightRef}
-	position={[$pointer.x * 15, $pointer.y * 2, 10]}
-	rotation.y={spotLightYRotation}
 	scale={scale.current}
 	onpointerdown={startDraggingState}
 	onpointerleave={endDraggingState}
-	onpointerup={endDraggingState}>
+	onpointerup={endDraggingState}
+	position={axis === 'x'
+		? [$pointer.x * 15, $pointer.y * 2, 10]
+		: [$pointer.x * 2, $pointer.y * 15, 10]}
+	{...axis === 'x'
+		? { rotation: [0, spotLightRotation, 0] }
+		: { rotation: [spotLightRotation, 0, 0] }}>
 	<RotationalLight angle={spotLightAngel} intensity={50} color={'white'} position={[0, 0, 0]} />
 </FlashLight>
