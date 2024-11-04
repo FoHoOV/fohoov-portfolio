26429e8b3b136a0ee5cfae0bf3c07be5d376509f
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-11-04 21:38:19 +0330

    refactor: index exports for some components

--- src/lib/components/home/Learning.svelte
+++ src/lib/components/home/Learning.svelte
@@ -1,6 +1,5 @@
 <script lang="ts" module>
-	import Item from '$lib/components/list/Item.svelte';
-	import List from '$lib/components/list/List.svelte';
+	import { List } from '$lib/components/list';
 
 	export type Props = {
 		class?: string;
@@ -11,24 +10,32 @@
 	const { class: className }: Props = $props();
 </script>
 
-<List class="flex flex-col lg:flex-row-reverse {className}" title="Learning">
+<List.Root class="flex flex-col lg:flex-row-reverse {className}" title="Learning">
 	{#snippet children({ popEffect })}
-		<Item
+		<List.Item
 			text="GSAP!"
 			level={6}
 			baseColor="green"
 			gsaps={[(options) => popEffect(options, true)]} />
 
-		<Item
+		<List.Item
 			text="Three.js (threlte)"
 			level={5}
 			baseColor="yellowgreen"
 			gsaps={[(options) => popEffect(options)]} />
 
-		<Item text="Next" level={'?'} baseColor="white" gsaps={[(options) => popEffect(options)]} />
+		<List.Item
+			text="Next"
+			level={'?'}
+			baseColor="white"
+			gsaps={[(options) => popEffect(options)]} />
 
-		<Item text="Go" level={'?'} baseColor="blue" gsaps={[(options) => popEffect(options)]} />
+		<List.Item text="Go" level={'?'} baseColor="blue" gsaps={[(options) => popEffect(options)]} />
 
-		<Item text="MongoDB" level={2} baseColor="green" gsaps={[(options) => popEffect(options)]} />
+		<List.Item
+			text="MongoDB"
+			level={2}
+			baseColor="green"
+			gsaps={[(options) => popEffect(options)]} />
 	{/snippet}
-</List>
+</List.Root>
